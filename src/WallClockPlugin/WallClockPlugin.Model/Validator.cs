2f141554905ee9cec30024df8bfcbc0bc04caac3
Tsukanov Semen <apple.semen@mail.ru>
2023-10-26 00:40:18 +0700

    feature(WallClockParameters): Добавлены поля и свойства, а так же пара приватных методов, для нахождения максимального и минимального значения зависимых параметров: длина минутной стрелки, длина часовой стрелки. Для валидации вхождения в диапазон, создан также класс Validator со статическим метолом ValidateRange

---
+++ src/WallClockPlugin/WallClockPlugin.Model/Validator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WallClockPlugin.Model
+{
+    /// <summary>
+    /// Класс для валидации данных
+    /// </summary>
+    public class Validator
+    {
+        #region [Public]
+        /// <summary>
+        /// Валидация входа значения в определенный диапазон [left;right]
+        /// </summary>
+        /// <param name="leftBorder"> Левая граница диапазона</param>
+        /// <param name="rightBorder"> Правая граница диапазона</param>
+        /// <param name="value"> Значение</param>
+        /// <returns> True - если значение входит в диапазон, false - если нет</returns>
+        public static bool ValidateRange(float leftBorder, float rightBorder, float value)
+        {
+            return value >= leftBorder && value <= rightBorder;
+        }
+
+        /// <summary>
+        /// Проверка на положительность значения
+        /// </summary>
+        /// <param name="value"> Значение</param>
+        /// <returns> True - если значение положительное или равное 0,
+        /// false - если значение отрицательное</returns>
+        public static bool CheckPositiveValue(float value)
+        {
+            return value >= 0;
+        }
+        #endregion
+    }
+}
---
+++ src/WallClockPlugin/WallClockPlugin.Model/WallClockParameters.cs
@@ -0,0 +1,223 @@
+﻿namespace WallClockPlugin.Model
+{
+    /// <summary>
+    /// Класс для хранения и валидации параметров
+    /// </summary>
+    public class WallClockParameters
+    {
+        #region [Private]
+        /// <summary>
+        /// Минимальное значение радиуса циферблата
+        /// </summary>
+        private const int MIN_RADIUS = 100;
+
+        /// <summary>
+        /// Максимальное значение радиуса циферблата
+        /// </summary>
+        private const int MAX_RADIUS = 200;
+
+        /// <summary>
+        /// Минимальное значение ширины бортика
+        /// </summary>
+        private const int MIN_SIDE_WIDTH = 30;
+
+        /// <summary>
+        /// Максимальное значение ширины бортика
+        /// </summary>
+        private const int MAX_SIDE_WIDTH = 60;
+
+        /// <summary>
+        /// Минимальное значение высоты бортика
+        /// </summary>
+        private const int MIN_SIDE_HEIGHT = 20;
+
+        /// <summary>
+        /// Максимальное значение высоты бортика
+        /// </summary>
+        private const int MAX_SIDE_HEIGHT = 40;
+
+        /// <summary>
+        /// Радиус циферблата
+        /// </summary>
+        private float _radius;
+
+        /// <summary>
+        /// Ширина бортика
+        /// </summary>
+        private float _sideWidth;
+
+        /// <summary>
+        /// Высота борткиа 
+        /// </summary>
+        private float _sideHeight;
+
+        /// <summary>
+        /// Длина минутной стрелки
+        /// </summary>
+        private float _minuteHandLength;
+
+        /// <summary>
+        /// Длина часовой стрелки
+        /// </summary>
+        private float _hourHandLength;
+
+        /// <summary>
+        /// Состояние - отображать только часы
+        /// </summary>
+        private bool _onlyHours = false;
+        #endregion
+
+        #region [Public]
+
+        /// <summary>
+        /// Радиус циферблата
+        /// </summary>
+        public float Radius 
+        {
+            get { return _radius; }
+
+            set 
+            {
+                if (!Validator.ValidateRange(MIN_RADIUS, MAX_RADIUS, value))
+                {
+                    throw new ArgumentException($"Input value is out of range - " +
+                        $"[{MIN_RADIUS};{MAX_RADIUS}]");
+                }
+
+                _radius = value;
+            }
+        }
+
+        /// <summary>
+        /// Ширина бортика
+        /// </summary>
+        public float SideWidth
+        {
+            get { return _sideWidth; }
+
+            set 
+            {
+                if(!Validator.ValidateRange(MIN_SIDE_WIDTH, MAX_SIDE_WIDTH, value))
+                {
+                    throw new ArgumentException($"Input value is out of range - " +
+                        $"[{MIN_SIDE_WIDTH};{MAX_SIDE_WIDTH}]");
+                }
+
+                _sideWidth = value;
+            }
+        }
+
+        /// <summary>
+        /// Высота бортика
+        /// </summary>
+        public float SideHeight
+        {
+            get { return _sideHeight; }
+
+            set 
+            {
+                if(!Validator.ValidateRange(MIN_SIDE_HEIGHT, MAX_SIDE_HEIGHT, value))
+                {
+                    throw new ArgumentException($"Input value is out of range - " +
+                        $"[{MIN_SIDE_HEIGHT};{MAX_SIDE_HEIGHT}]");
+                }
+
+                _sideHeight = value;
+            }
+        }
+
+        /// <summary>
+        /// Длина минутной стрелки
+        /// </summary>
+        public float MinuteHandLength
+        {
+            get { return _minuteHandLength; }
+
+            set 
+            {
+                var minValue = MinMinuteHandLength();
+                var maxValue = MaxMinuteHandLength();
+
+                if(!Validator.ValidateRange(minValue, maxValue, value))
+                {
+                    throw new ArgumentException($"Input value is out of range - " +
+                        $"[{minValue};{maxValue}]");
+                }
+
+                _minuteHandLength = value;
+            }
+        }
+
+        /// <summary>
+        /// Длина часовой стрелки
+        /// </summary>
+        public float HourHandLength
+        {
+            get { return _hourHandLength; }
+
+            set 
+            {
+                var minValue = MinHourHandLength();
+                var maxValue = MaxHourHandLength();
+
+                if(!Validator.ValidateRange(minValue, maxValue, value))
+                {
+                    throw new ArgumentException($"Input value is out of range - " +
+                        $"[{minValue};{maxValue}]");
+                }
+
+                _hourHandLength = value;
+            }
+        }
+
+        /// <summary>
+        /// Состояние - отображать только часы
+        /// </summary>
+        public bool OnlyHours { get; set; }
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        WallClockParameters() { }
+        #endregion
+
+        #region [Private methods]
+
+        /// <summary>
+        /// Метод возвращает минимальное значение длины минутной стрелки
+        /// </summary>
+        /// <returns> Минимальное значение длины минутной стрелки</returns>
+        private float MinMinuteHandLength()
+        {
+            return (_radius / 2) + 4;
+        }
+
+        /// <summary>
+        /// Метод возвращает минимальное значение длины часовой стрелки
+        /// </summary>
+        /// <returns> Минимальное значение длины часовой стрелки</returns>
+        private float MinHourHandLength()
+        {
+            return _radius / 5;
+        }
+
+        /// <summary>
+        /// Метод возвращает максимальне значение длины минутной стрелки
+        /// </summary>
+        /// <returns> Максимальное значение длины минутной стрелки</returns>
+        private float MaxMinuteHandLength()
+        {
+            return _radius - 2;
+        }
+
+        /// <summary>
+        /// Метод возвращает максимальное значение длины часовой стрелки
+        /// </summary>
+        /// <returns> Максимальное значение длины часовой стрелки</returns>
+        private float MaxHourHandLength()
+        {
+            return _minuteHandLength / 2;
+        }
+        #endregion
+    }
+}
