eefd2fa3048d3a156e2e792155432d3f9298d85f
Tsukanov Semen <apple.semen@mail.ru>
2023-11-21 11:16:49 +0700

    tests(Validator.cs): Написаны тесты для класса Validator.cs

--- src/WallClockPlugin/WallClockPlugin.Model.UnitTests/WallClockParametersTests.cs
+++ src/WallClockPlugin/WallClockPlugin.Model.UnitTests/WallClockParametersTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WallClockPlugin.Model.UnitTests
+﻿namespace WallClockPlugin.Model.UnitTests
 {
+    using System;
     using NUnit.Framework;
     using WallClockPlugin.Model;
 
